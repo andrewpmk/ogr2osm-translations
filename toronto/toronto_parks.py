75fbcac79dd2ae73aeb5da6928c14292b5f120d4
andrewpmk <andrewpmk@gmail.com>
2015-07-22 16:33:21 -0400

    add rules for parks

---
+++ toronto/toronto_parks.py
@@ -0,0 +1,28 @@
+"""
+Translation rules for City of Toronto Open Data, Parks.
+http://opendata.toronto.ca/gcc/city_green_space_wgs84.zip
+
+Copyright 2015 Andrew MacKinnon, 2011 Paul Norman.
+
+WORK IN PROGRESS - DO NOT USE
+
+"""
+
+def translateName(rawname):
+	newName = rawname.title()
+	return newName.strip()
+
+def filterTags(attrs):
+	if not attrs: return
+
+	tags = {}
+	
+	#Add the source
+	tags.update({'source':'City of Toronto Open Data'})
+	#Add leisure=park
+	tags.update({'leisure':'park'})
+	#automagically convert names
+	if attrs['LF_NAME']:
+		tags.update({'name':translateName(attrs['LF_NAME'].strip(' '))})
+
+	return tags
