ca81fc5571fbb665c965f38fb16d0b0d4b9b7b89
andrewpmk <andrewpmk@gmail.com>
2015-07-23 12:47:49 -0400

    more road types

--- york_region/roads.py
+++ york_region/roads.py
@@ -3,6 +3,12 @@ Translation rules for York Region Open Data, Roads.
 
 Copyright 2015 Andrew MacKinnon, 2011 Paul Norman.
 
+Projection is EPSG:26917
+
+TODO:
+Remove Unopened Road Allowance
+Remove Proposed Road (maybe)
+
 WORK IN PROGRESS - DO NOT USE
 
 """
@@ -27,8 +33,21 @@ def filterTags(attrs):
 		tags.update({'highway':'residential'})
 	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Regional Road":
 		tags.update({'highway':'secondary'})
+	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Arterial Road":
+		tags.update({'highway':'secondary'})
+	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Interchange-Regional Road":
+		tags.update({'highway':'secondary'})
+	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Interchange-Urban Road":
+		tags.update({'highway':'residential'})
+	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Interchange-Prov Highway":
+		tags.update({'highway':'secondary_link'})
+	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Rural Road":
+		tags.update({'highway':'unclassified'})
 	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Proposed Road":
 		tags.update({'highway':'proposed'})
+	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Accessway":
+		tags.update({'highway':'service'})
+		tags.update({'service':'driveway'})
 	elif attrs['ROAD_CLASS'] and attrs['ROAD_CLASS'] == "Laneway":
 		tags.update({'highway':'service'})
 		tags.update({'service':'alley'})
@@ -38,7 +57,8 @@ def filterTags(attrs):
 	else:
 		tags.update({'highway':'road'})
 	if 'ROAD_NUMBE' in attrs:
-		tags.update({'ref': attrs['ROAD_NUMBE'].strip(' ')})
+		if attrs['ROAD_NUMBE']!='':
+			tags.update({'ref': attrs['ROAD_NUMBE'].strip(' ')})
 	if 'ROAD_CLASS' in attrs:
 		tags.update({'opendata:type': attrs['ROAD_CLASS'].strip(' ')})
 	if 'FLOW' in attrs:
