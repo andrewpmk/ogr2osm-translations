38fe6d2830c0b39f33e8a68654ec53fd799cb52d
andrewpmk <andrewpmk@gmail.com>
2015-07-22 21:36:55 -0400

    remove WORK IN PROGRESS - DO NOT USE

--- toronto/toronto_schools.py
+++ toronto/toronto_schools.py
@@ -2,7 +2,6 @@
 Translation rules for City of Toronto Open Data, Schools.
 http://opendata.toronto.ca/gcc/school_tdsb_wgs84.zip
 Copyright 2015 Andrew MacKinnon, 2011 Paul Norman.
-WORK IN PROGRESS - DO NOT USE
 """
 
 def filterTags(attrs):
